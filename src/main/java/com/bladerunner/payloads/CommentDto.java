74dd9dba2eafc69a88a6b42a8a954707e8494f15
SachinDabral <dabral.sachin40@gmail.com>
2022-09-27 13:09:57 +0530

    updated

--- src/main/java/com/bladerunner/payloads/CommentDto.java
+++ src/main/java/com/bladerunner/payloads/CommentDto.java
@@ -1,8 +1,5 @@
 package com.bladerunner.payloads;
 
-import com.bladerunner.entities.Post;
-import com.bladerunner.entities.User;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
